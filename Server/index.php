7b4fd9f5a031975795fb48a0c75c580b38a0234b
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-04-09 07:55:57 -0500

    Formulario PHP

--- Server/index.php
+++ Server/index.php
@@ -5,6 +5,15 @@
     <title>Document</title>
 </head>
 <body>
-   <?php echo 'Hola mundo'; ?>
+  <h1>Formulario:</h1>
+  <h1> <?php echo 'Página en php' ?></h1>
+
+  <br>
+
+  <form action="respuesta.php" method="get">
+      <input type="text" name="usser" id="usser" placeholder="Usuario">
+      <input type="password" name="pass" id="pass" placeholder="Contraseña">
+      <input type="submit" value="Enviar">
+  </form>
 </body>
 </html>
