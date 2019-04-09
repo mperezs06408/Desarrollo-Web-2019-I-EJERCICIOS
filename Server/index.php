13f1cdbdeacd8ac1b88162bf0e60ac00e77bbca1
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-04-09 08:14:23 -0500

    Imprimir datos en nueva ventana

--- Server/index.php
+++ Server/index.php
@@ -5,11 +5,18 @@
     <title>Document</title>
 </head>
 <body>
+ <!--
+ <?php
+    $usuarios = [['mperez','1234']]
+
+    ?>
+    -->
   <h1>Formulario:</h1>
   <h1> <?php echo 'Página en php' ?></h1>
 
   <br>
 
+  <!-- formulario para enviar datos desde el server con php -->
   <form action="respuesta.php" method="get">
       <input type="text" name="usser" id="usser" placeholder="Usuario">
       <input type="password" name="pass" id="pass" placeholder="Contraseña">
