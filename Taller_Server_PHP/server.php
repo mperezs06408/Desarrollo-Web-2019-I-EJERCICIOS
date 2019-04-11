720d71fc624f22f18e088a5f04e1e09d4e750ca9
mperezs06408 <47216565+mperezs06408@users.noreply.github.com>
2019-04-11 09:50:19 -0500

    Update server.php

--- Taller_Server_PHP/server.php
+++ Taller_Server_PHP/server.php
@@ -1,3 +1,20 @@
 <?php
-
+    $users = [ 
+        'santiago' => '1234',
+        'natalia' => '9876',
+    ];
+    
+    if (isset($users[$_GET['user']])){
+        if ($users[$_GET['user']]== $_GET['password']){
+            session_start();
+            echo 'sesiòn iniciada';
+            
+        } else {
+            header('location:index.php');
+            echo 'Contraseña incorrecta';
+        }
+    } else {
+        header('location:index.php');
+        echo 'datos incorrectos';
+    }
 ?>
