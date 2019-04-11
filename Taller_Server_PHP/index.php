30039d47e584064a86952b08c8dce038d9fdb1f0
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-04-11 09:14:07 -0500

    Nuevo server

---
+++ Taller_Server_PHP/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio de sesión</title>
+</head>
+<body>
+    <h1>Inicio de sesión</h1>
+    <div>
+        <form action="server.php" method="post">
+            <input type="text" name="user" id="user" placeholder="USUARIO">
+            <input type="password" name="password" id="password" placeholder="CONTRASEÑA">
+            <input type="submit" value="send">
+        </form>
+    </div>
+</body>
+</html>
